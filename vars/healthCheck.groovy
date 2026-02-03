f305d7d03da7295e246e667c102f49b14c708c7d
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2026-02-03 10:56:04 +0530

    Update healthCheck.groovy

--- vars/healthCheck.groovy
+++ vars/healthCheck.groovy
@@ -1,6 +1,13 @@
 
-def call(String url) {
+/*def call(String url) {
     stage('Health Check') {
         sh "curl -f ${url}"
     }
+}
+*/
+
+def call(String deploymentName = "stockmanager-blue") {
+    sh """
+      kubectl rollout status deployment/${deploymentName} --timeout=120s
+    """
 }
