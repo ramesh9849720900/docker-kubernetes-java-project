20e850a32fa0574a99bcfb9b4b30da2d42d814f5
ramesh9849720900 <114875739+ramesh9849720900@users.noreply.github.com>
2026-02-03 06:30:52 +0530

    Update dockerBuildPush.groovy

--- vars/dockerBuildPush.groovy
+++ vars/dockerBuildPush.groovy
@@ -1,1 +1,13 @@
 
+def call(String appDir, String imageTag, String ecrRepo) {
+    stage('Docker Build & Push') {
+        dir(appDir) {
+            sh """
+              docker build -t ${ecrRepo}:${imageTag} .
+              aws ecr get-login-password --region eu-north-1 \
+                | docker login --username AWS --password-stdin ${ecrRepo}
+              docker push ${ecrRepo}:${imageTag}
+            """
+        }
+    }
+}
